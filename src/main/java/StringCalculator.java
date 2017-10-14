ad977e78d3b504569792428ddb332a4119e70d95
Wojtek Pietrucha <wojtekpietrucha@gmail.com>
2017-10-14 11:12:55 +0200

    Added implemntation for one number.

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,3 +1,15 @@
-
 public class StringCalculator {
+
+    public static final String EMPTY_STRING = "";
+
+    public int Add(String numbers) {
+        if (isEmpty(numbers)) {
+            return 0;
+        }
+        return Integer.valueOf(numbers);
+    }
+
+    private boolean isEmpty(String numbers) {
+        return EMPTY_STRING.equals(numbers);
+    }
 }
