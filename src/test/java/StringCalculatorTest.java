c39275629d7afdee5c3de7563e45e4e884d861e3
Bartosz-global <bartosz.maleta@gmail.com>
2017-10-14 13:33:06 +0200

    Add

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,5 +1,6 @@
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,6 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  * Tdd training on 14.10.17.
  */
 @RunWith(JUnitParamsRunner.class)
+
 public class StringCalculatorTest {
 
     private StringCalculator testedObject;
@@ -25,37 +27,84 @@ public class StringCalculatorTest {
     }
 
     @Test
-    public void shouldReturnZeroWhenEmptyStringPassed() {
+    public void shouldReturnZeroWhenEmptyStringPassed()
+    {
         int result = testedObject.Add("");
 
         assertThat(result).isEqualTo(0);
     }
 
     @Test
+    public void shouldReturnZeroWhenZeroIsPassed()
+    {
+        int result = testedObject.Add("0");
+
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    public void shouldReturnZeroWhenOneIsPassed()
+    {
+        int result = testedObject.Add("1");
+
+        assertThat(result).isEqualTo(1);
+
+
+       // assertEquals(result).isEqualTo(1);
+    }
+
+    @Test
     @Parameters({"0,0", "1,1", ",0"})
-    public void shouldReturnCorrectValueWhenOneNumberPassed(String input,
-                                                            int expectedOutput) {
+    public void shouldReturnCorrectValueWhenOneNumberPassed(String input, int expectedOutput)
+    {
         int result = testedObject.Add(input);
 
         assertThat(result).isEqualTo(expectedOutput);
     }
 
-    private Object[][] parametersForTestingNonStandardInput() {
-        return new Object[][]{
-                {"", 0},
-                {"1", 1}
-        };
+    private Object[][] parametersForTestingNonStandardInput()
+    {
+        return new Object[][]
+                {
+                        {"", 0},
+                        {"1", 1},
+                        {"1,2,3", 6}
+                };
     }
+
+//    @Test
+//    @Parameters({"0,0", "1,1", ",0"})
+//    public void shouldReturnCorrectValueWhenNumbersAdded(int input, int output)
+//    {
+//        int result = testedObject.Add(1, 1);
+//
+//        assertThat(result).isEqualTo(result);
+//    }
+//
+//    private Object[][] parametersForTestingNonStandardInput()
+//    {
+//        return new Object[][]
+//                {
+//                        {"", 0},
+//                        {"1", 1},
+//                };
+//    }
+
+
+
+
+
 
     @Test
     @Parameters(method = "parametersForTestingNonStandardInput")
     public void shouldReturnCorrectValueWhenNonStandardInputPassed(
-            String input, int expectedOutput
-    ) {
+            String input, int expectedOutput)
+    {
         int result = testedObject.Add(input);
 
         assertThat(result).isEqualTo(expectedOutput);
     }
+
 
 
 }
