8d36a45e89cda9b3af527ce1c00608faaf05f380
Andrzej <andrzej.chudzicki@onet.pl>
2017-10-14 15:12:12 +0200

    added Exception handle

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -5,22 +5,35 @@ public class StringCalculator {
     public int Add(String numbers) {
         if (isEmpty(numbers)) {
             return 0;
-        } else if (numbers.length() != 1) {
+        } else if (isValid(numbers) && numbers.length() != 1) {
             return calculation(numbers);
         } else
             return Integer.valueOf(numbers);
     }
 
     private int calculation(String numbers) {
-        String[] eachNumber = numbers.split(",");
         int sum = 0;
-        for (int i = 0; i < eachNumber.length; i++) {
-            sum += Integer.valueOf(eachNumber[i]);
+
+        for (String number : numbers.split("[,\n]")) {
+            sum += Integer.parseInt(number);
         }
+
         return sum;
     }
 
     private boolean isEmpty(String numbers) {
         return EMPTY_STRING.equals(numbers);
     }
+
+    private boolean isValid (String numbers) {
+        for (String number: numbers.split("[,\n]")) {
+          try {
+              Integer.parseInt(number);
+          } catch (NumberFormatException e) {
+              throw new NumberFormatException("Only numbers and comas expected!");
+          }
+        }
+        return true;
+    }
+
 }
