f29f4e8f91d30c844bb50a259f72577b547de509
Kamil <kbrzoza1994@gmail.com>
2017-10-14 13:25:57 +0200

    first branch commit

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,3 +1,25 @@
-
 public class StringCalculator {
+    public static final String EMPTY_STRING = "";
+    public int Add(String numbers) {
+        if (isEmpty(numbers)) {
+            return 0;
+        }
+        else if (numbers.length()==1)
+        return Integer.valueOf(numbers);
+        else if (numbers.length()==3){
+            String[] tab = numbers.split(",");
+            return (Integer.valueOf(tab[0])+Integer.valueOf(tab[1])) ;
+        }else {
+            Integer res = 0;
+            String[] tab = numbers.split(",");
+
+            for (int i = 0; i<tab.length;i++){
+                res+=Integer.valueOf(tab[i]);
+            }
+            return res;
+        }
+    }
+    private boolean isEmpty(String numbers) {
+        return EMPTY_STRING.equals(numbers);
+    }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,22 +1,70 @@
-import org.junit.Before;
-import org.junit.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
+import junitparams.JUnitParamsRunner;
+import org.junit.runner.RunWith;
+        import junitparams.Parameters;
+        import org.junit.Before;
+        import org.junit.Test;
+        import org.junit.runner.RunWith;
+        import static org.assertj.core.api.Assertions.assertThat;
 /**
  * Tdd training on 14.10.17.
  */
+@RunWith(JUnitParamsRunner.class)
 public class StringCalculatorTest {
-
     private StringCalculator testedObject;
-
     @Before
     public void setUp() throws Exception {
         testedObject = new StringCalculator();
     }
-
     @Test
     public void shouldCreateObject() throws Exception {
         assertThat(testedObject).isNotNull();
     }
+    @Test
+    public void shouldReturnZeroWhenEmptyStringPassed() {
+        int result = testedObject.Add("");
+        assertThat(result).isEqualTo(0);
+    }
+    @Test
+    @Parameters({"0,0", "1,1", ",0"})
+    public void shouldReturnCorrectValueWhenOneNumberPassed(String input,
+                                                            int expectedOutput) {
+        int result = testedObject.Add(input);
+        assertThat(result).isEqualTo(expectedOutput);
+    }
+    private Object[][] parametersForTestingNonStandardInput() {
+        return new Object[][]{
+                {"", 0},
+                {"1", 1},
+                {"1,8", 9},
+                {"14,98", 112},
+                {"14,15,29", 58},
+                {"41,1,12",54}
+        };
+    }
+
+    @Test
+    @Parameters(method = "parametersForTestingNonStandardInput")
+    public void shouldReturnCorrectValueWhenNonStandardInputPassed(
+            String input, int expectedOutput
+    ) {
+        int result = testedObject.Add(input);
+        assertThat(result).isEqualTo(expectedOutput);
+    }
+
+    @Test
+    @Parameters(method = "parametersForTestingNonStandardInput" )
+    public void shouldReturnCorrectValueWhenStandardInputPassed(String input,
+                                                            int expectedOutput) {
+        int result = testedObject.Add(input);
+        assertThat(result).isEqualTo(expectedOutput);
+    }
+    @Test
+    @Parameters(method = "parametersForTestingNonStandardInput" )
+    public void shouldReturnCorrectValueWhenMoreThatTwoInputPassed(String input,
+                                                                int expectedOutput) {
+        int result = testedObject.Add(input);
+        assertThat(result).isEqualTo(expectedOutput);
+    }
+
+
 }
