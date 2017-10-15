689c30be82932d9d2a6400038107c1f10b9a34c5
romanmarczewski <dharma@codemandala.com>
2017-10-15 11:27:14 +0200

    Exception balance exceeded to much thrown

--- src/main/java/bank/PaymentService.java
+++ src/main/java/bank/PaymentService.java
@@ -4,8 +4,14 @@ package bank;
  * Tdd training on 15.10.17.
  */
 public class PaymentService {
-    public void transferMoney(Account accountFrom, Account accountTo, int amountToTransfer) {
-        accountFrom.withdraw(amountToTransfer);
-        accountTo.deposit(amountToTransfer);
+    public void transferMoney(Account accountFrom,
+                              Account accountTo,
+                              int amountToTransfer) throws IllegalArgumentException {
+        System.out.println((accountFrom.getBalance() - amountToTransfer));
+        if ((accountFrom.getBalance()-amountToTransfer)>-501) {
+                accountFrom.withdraw(amountToTransfer);
+                accountTo.deposit(amountToTransfer);
+            } else throw new IllegalArgumentException("You no understand me, " +
+                "your balance is exceeded to much beyond -500");
     }
 }
--- src/test/java/bank/PaymentServiceTest.java
+++ src/test/java/bank/PaymentServiceTest.java
@@ -1,7 +1,9 @@
 package bank;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -9,6 +11,9 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
  * Tdd training on 15.10.17.
  */
 public class PaymentServiceTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     private PaymentService testedObject;
 
@@ -27,5 +32,31 @@ public class PaymentServiceTest {
         assertThat(accountFrom.getBalance()).isEqualTo(7);
         assertThat(accountTo.getBalance()).isEqualTo(3);
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionWhenNegativeBalanceExceededToMuch() {
+        int amount = 10;
+        Account accountFrom = new Account(1, -495);
+        Account accountTo = new Account(2, 0);
+
+        testedObject.transferMoney(accountFrom, accountTo, amount);
+        thrown.expect(IllegalArgumentException.class);
+
+    }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionMessageWhenNegativeBalanceExceededToMuch() {
+        int amount = 10;
+        Account accountFrom = new Account(1, -495);
+        Account accountTo = new Account(2, 0);
+
+        testedObject.transferMoney(accountFrom, accountTo, amount);
+        thrown.expectMessage("You no understand me, " +
+                "your balance is exceeded to much beyond -500");
+    }
+
+
+
 
 }
