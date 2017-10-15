42e3d5c943d42f000e6ae527500d2af9255d198c
Tomasz S <tomasz.z.seidler@gmail.com>
2017-10-15 10:45:58 +0200

    not enough money in accountFrom throws: IllegalArgumentException with message "I'm very sorry, but you don't have enough money..."

--- src/main/java/bank/PaymentService.java
+++ src/main/java/bank/PaymentService.java
@@ -5,7 +5,13 @@ package bank;
  */
 public class PaymentService {
     public void transferMoney(Account accountFrom, Account accountTo, int amountToTransfer) {
-        accountFrom.withdraw(amountToTransfer);
-        accountTo.deposit(amountToTransfer);
+        if (isEnouthMoney(accountFrom, amountToTransfer)) {
+            accountFrom.withdraw(amountToTransfer);
+            accountTo.deposit(amountToTransfer);
+        } else throw new IllegalArgumentException("I'm very sorry, but you don't have enough money...");
+    }
+
+    private boolean isEnouthMoney(Account accountFrom, int amountToTransfer) {
+        return accountFrom.getBalance() >= amountToTransfer;
     }
 }
