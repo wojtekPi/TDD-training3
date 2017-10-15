a717efb9a12a1a0ed3ebc7bbf2bb7005a27c6087
szczepanskikrs <szczepanskikrs@gmail.com>
2017-10-15 12:30:58 +0200

    Version 0.2 fixed

--- src/main/java/bank/PaymentService.java
+++ src/main/java/bank/PaymentService.java
@@ -4,11 +4,17 @@ package bank;
  * Tdd training on 15.10.17.
  */
 public class PaymentService {
-    public void transferMoney(Account accountFrom, Account accountTo, int amountToTransfer) {
-        checkIfAccountBalanceIsValid(accountFrom, amountToTransfer);
-        accountFrom.withdraw(amountToTransfer);
-        accountTo.deposit(amountToTransfer);
+    public void transferMoney(Account accountFrom, Account accountTo, Instrument instrument) {
+        checkIfAccountBalanceIsValid(accountFrom, instrument.getAmount());
+        checkIfSameCurrency(accountFrom, accountTo);
+        accountFrom.withdraw(instrument.getAmount());
+        accountTo.deposit(instrument.getAmount());
+    }
 
+    private void checkIfSameCurrency(Account accountFrom, Account accountTo) {
+        if (!accountFrom.getInstrument().getCurrencies().equals(accountTo.getInstrument().getCurrencies())){
+            throw new IllegalArgumentException("Wrong currency!");
+        }
     }
 
     private void checkIfAccountBalanceIsValid(Account accountFrom, int amountToTransfer) {
