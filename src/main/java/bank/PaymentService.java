daac56144e562765b0b40e8f33f00a0ff190e952
tblazejowski <tomasz.blazejowski@gmail.com>
2017-10-15 12:26:33 +0200

    version 0.2 for different currencies with Instrument added

--- src/main/java/bank/PaymentService.java
+++ src/main/java/bank/PaymentService.java
@@ -4,10 +4,11 @@ package bank;
  * Tdd training on 15.10.17.
  */
 public class PaymentService {
-    public void transferMoney(Account accountFrom, Account accountTo, int amountToTransfer) {
-        checkIfAccountBalanceIsValid(accountFrom, amountToTransfer);
-        accountFrom.withdraw(amountToTransfer);
-        accountTo.deposit(amountToTransfer);
+    public void transferMoney(Account accountFrom, Account accountTo, Instrument instrument) {
+        checkIfAccountBalanceIsValid(accountFrom, instrument.amount);
+        checkIfAccountCurrencyIsValid(accountFrom, accountTo, instrument);
+        accountFrom.withdraw(instrument.amount);
+        accountTo.deposit(instrument.amount);
 
     }
 
@@ -15,5 +16,10 @@ public class PaymentService {
         if (accountFrom.getBalance() - amountToTransfer < -500) {
             throw new IllegalArgumentException("I'm very sorry but you don't have enough money...");
         }
+    }
+
+    private void checkIfAccountCurrencyIsValid(Account accountFrom, Account accountTo, Instrument instrument) {
+        if ((accountFrom.getCurrency() != accountTo.getCurrency()) || (instrument.currency != accountFrom.getCurrency()))
+            throw new IllegalArgumentException("Currencies do not match!");
     }
 }
