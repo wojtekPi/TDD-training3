cafc0148aa0d07122f40277e39d0e1d9c5f47d68
Andrzej <andrzej.chudzicki@onet.pl>
2017-10-15 11:27:13 +0200

    Added handling with exception when not enough money

--- src/test/java/bank/PaymentServiceTest.java
+++ src/test/java/bank/PaymentServiceTest.java
@@ -1,7 +1,10 @@
 package bank;
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -11,6 +14,8 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class PaymentServiceTest {
 
     private PaymentService testedObject;
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     @Before
     public void setUp() throws Exception {
@@ -26,6 +31,29 @@ public class PaymentServiceTest {
 
         assertThat(accountFrom.getBalance()).isEqualTo(7);
         assertThat(accountTo.getBalance()).isEqualTo(3);
+    }
+
+    @Test
+    public void shouldThrowIllegalArgumentExceptionWhenBalanceIsTooLow() {
+        thrown.expect(IllegalArgumentException.class);
+        Account accountFrom = new Account(1, 0);
+        Account accountTo = new Account(2, 0);
+        thrown.expectMessage("I'm very sorry, but you dont;t have enough money...");
+        testedObject.transferMoney(accountFrom, accountTo, 600);
+    }
+
+    @Test
+    public void shouldReturnTrueIfBalanceNotChange() {
+        Account accountFrom = new Account(1, 0);
+        int balanceBefore = accountFrom.getBalance();
+        Account accountTo = new Account(2, 0);
+        try {
+            testedObject.transferMoney(accountFrom, accountTo, 600);
+        } catch (IllegalArgumentException e) {
+        }
+        int balanceAfter = accountFrom.getBalance();
+        assertThat(balanceBefore).isEqualTo(balanceAfter);
+
     }
 
 }
